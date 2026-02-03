c65b59a4168f864568840f88ee4f14cb4e90d249
AnshulKumar001 <anshulkumar9368289674@gmail.com>
2026-02-03 23:17:06 +0530

    added new problem Max sum of subarray of size k

--- SlidingWindow/problem01.py
+++ SlidingWindow/problem01.py
@@ -1,7 +1,14 @@
 #Max sum of subarray of size k
 
-arr=[1,2,3,4,5,6,9]
-k=3
-window_sum=sum(arr[:k])
-max_sum=window_sum
+arr = [2, 1, 5, 1, 3, 2]
+k = 3
 
+window_sum = sum(arr[:k])
+max_sum = window_sum
+
+for i in range(k, len(arr)):
+    window_sum += arr[i]       # add next
+    window_sum -= arr[i-k]     # remove previous
+    max_sum = max(max_sum, window_sum)
+
+print(max_sum)  # 9
