e277d753208be3bfad94bb7a3289d7ff125d3399
AnshulKumar001 <anshulkumar9368289674@gmail.com>
2026-02-23 23:27:29 +0530

    modify problem

--- SlidingWindow/problem03.py
+++ SlidingWindow/problem03.py
@@ -4,33 +4,33 @@ Output: 15
 Input: nums = [4,4,4], k = 3
 Output: 0'''
 
-class Solution(object):
-    def maximumSubarraySum(self, nums, k):
-        seen = set()
-        win_sum = 0
-        max_sum = 0
-        left = 0
+nums = [1,5,4,2,9,9,9]
+k = 3
 
-        for right in range(len(nums)):
-            
-            # agar duplicate aaye to window shrink karo
-            while nums[right] in seen:
-                seen.remove(nums[left])
-                win_sum -= nums[left]
-                left += 1
+seen = set()
+window_sum = 0
+max_sum = 0
+left = 0
 
-            # add new element
-            seen.add(nums[right])
-            win_sum += nums[right]
+for right in range(len(nums)):
 
-            # window size k ho jaye
-            if right - left + 1 == k:
-                max_sum = max(max_sum, win_sum)
+    # duplicate aaye to window shrink
+    while nums[right] in seen:
+        seen.remove(nums[left])
+        window_sum -= nums[left]
+        left += 1
 
-                # slide window forward
-                seen.remove(nums[left])
-                win_sum -= nums[left]
-                left += 1
+    # new element add
+    seen.add(nums[right])
+    window_sum += nums[right]
 
-        return max_sum
-   
+    # window size k ho gayi
+    if right - left + 1 == k:
+        max_sum = max(max_sum, window_sum)
+
+        # window slide
+        seen.remove(nums[left])
+        window_sum -= nums[left]
+        left += 1
+
+print(max_sum)
