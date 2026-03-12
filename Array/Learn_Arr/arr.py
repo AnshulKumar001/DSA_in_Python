c66a87ff3de8c8927e2c4ce4458a28b8045f460b
AnshulKumar001 <anshulkumar9368289674@gmail.com>
2026-03-12 23:32:02 +0530

    added some basic operatin

--- Array/Learn_Arr/arr.py
+++ Array/Learn_Arr/arr.py
@@ -1,3 +1,41 @@
 arr=[2,3,4,5,90]
+print(arr)
+#index
 print(arr[1])
 
+
+#2D Array
+arr2D=[[2,3,4],[3,4,5]]
+print(arr2D)
+#index
+print(arr2D[1][1])
+
+
+
+#Linear traverse
+arr=[2,3,4,5,90]
+for i in arr:
+    print(i,end=" ")
+
+print("\n")
+
+#Reverse
+arr=[2,3,4,5,90]
+for i in range(len(arr)-1,-1,-1):
+    print(arr[i],end=" ")
+
+print("\n")
+
+arr=[2,3,4,5,90]
+i=0
+while i<len(arr):
+    print(arr[i],end=" ")   
+    i += 1 
+
+print("\n")
+
+#insert
+arrr=[3,4,34,643]
+elem=50
+arrr.insert(0,elem)
+print(arrr)
