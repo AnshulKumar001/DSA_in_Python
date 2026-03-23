77dbd0418fe39fdf24c17bf17ca8ef962f8651df
AnshulKumar001 <anshulkumar9368289674@gmail.com>
2026-03-23 22:41:16 +0530

    added new problem of rotate matrix in array

---
+++ Array/matrix_rotate.py
@@ -0,0 +1,16 @@
+class Solution():
+    def Rotate_matrix(self, mat):
+        n=len(mat)
+        for i in range (n):
+            for j in range (1,n):
+                mat[i][j],mat[j][i]=mat[j][i],mat[i][j]
+        for row in mat:
+            row.reverse()
+
+mat=[[1,2,3],
+     [4,5,6],
+     [7,8,9]]  
+sol=Solution()
+sol.Rotate_matrix(mat)
+for row in mat:
+    print(row)            
